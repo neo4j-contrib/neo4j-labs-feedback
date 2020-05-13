042cbbd86c42af4a26454c56696ebf16041aaf8d
Mark Needham <m.h.needham@gmail.com>
2020-05-13 16:54:51 +0100

    Updates

---
+++ precache-manifest.7366b39fa731f853d8a61f6279d0c3e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f242cf70de7bc2a878873bc1764cec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb7b5e085838e18ab3ff",
+    "url": "/static/css/main.21755ad4.chunk.css"
+  },
+  {
+    "revision": "ad46316179c048611ce5",
+    "url": "/static/js/2.1689192a.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.1689192a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7b5e085838e18ab3ff",
+    "url": "/static/js/main.1ec41a2f.chunk.js"
+  },
+  {
+    "revision": "e230de3b94593a149ae9",
+    "url": "/static/js/runtime-main.30cff0ef.js"
+  }
+]);
