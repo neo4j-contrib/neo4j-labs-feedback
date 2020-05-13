61c029a5d45a99dd657b8aa2bd29e0267084db64
Mark Needham <m.h.needham@gmail.com>
2020-05-13 11:08:07 +0100

    dropdown to select project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import {Container, Header, Icon, Image, Menu, Segment} from "semantic-ui-react";
+import {Container, Dropdown, Header, Icon, Image, Menu, Segment} from "semantic-ui-react";
 import labsImage from './labs.png';
 import {Location, navigate, Redirect, Router} from "@reach/router";
 import {Feedback} from "./Feedback";
@@ -31,8 +31,18 @@ const topBarStyle = {
 
 function SideMenu(props) {
   const project = props.project
+
+  const options = [
+    { key: "apoc", text: 'APOC', value: "apoc" },
+    { key: "neo4j-streams", text: 'Neo4j Streams', value: "neo4j-streams" },
+  ]
+
   return <Menu vertical={true} inverted style={menuStyle}>
     <div style={topBarStyle}>
+      <Menu.Item>
+        <Dropdown options={options} fluid defaultValue={project} onChange={(event, data) => navigate(`/${data.value}`)} />
+      </Menu.Item>
+
       <Menu.Item as='a' onClick={() => navigate(`/${project}`)}
                  style={defaultIconStyle}>
         <Icon size='big' name='home' color='grey'/>
